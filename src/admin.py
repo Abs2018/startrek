3fc3856c99ebcd5ae96aa749ceac9692706891dd
Alex Kurt <akurt@yellowcanary.ai>
2022-05-16 13:15:31 -0400

    Worked on Big Bang

--- src/admin.py
+++ src/admin.py
@@ -14,9 +14,9 @@ from modules import bigbang as bb
 
 connection = db.stdb()
 if connection == "FALSE":
-    # The database has not been installed. Display setup menu.
-    art.cd(22, '', "First time setup detected.", "reset", True)
-    setup.setup()
+	# The database has not been installed. Display setup menu.
+	art.cd(22, '', "First time setup detected.", "reset", True)
+	setup.setup()
 
 # Continue to menu.
 '''
@@ -33,54 +33,64 @@ Characters from https://en.wikipedia.org/wiki/Code_page_437
 
 
 def adm_main_menu():
-    print("")
-    art.cd(226, '', "\tSTAR TREK GAME CONFIGURATION MENU", "", True)
-    print("")
-    # Row 1
-    art.cd(5, '', "\t<", "", False)
-    art.cd('light_cyan', '', "B", "", False)
-    art.cd(5, '', "> ", "", False)
-    art.cd(2, '', "The Big Bang", "", False)
+	print("")
+	art.cd(226, '', "\tSTAR TREK GAME CONFIGURATION MENU", "", True)
+	print("")
+	# Row 1
+	# If the galaxy has been created, do not show this.
+	connection = db.stdb()
+	query = "select * from `stars` LIMIT 1"
+	results = db.query(connection,query)
+	if results != False:
+		art.cd(5, '', "\t<", "", False)
+		art.cd('light_cyan', '', "B", "", False)
+		art.cd(5, '', "> ", "", False)
+		art.cd(2, '', "Delete Galaxy", "", False)
+	else:
+		art.cd(5, '', "\t<", "", False)
+		art.cd('light_cyan', '', "B", "", False)
+		art.cd(5, '', "> ", "", False)
+		art.cd(2, '', "The Big Bang", "", False)
 
-    art.cd(5, '', "\t<", "", False)
-    art.cd('light_cyan', '', "S", "", False)
-    art.cd(5, '', "> ", "", False)
-    art.cd(2, '', "Sector Editor", "", True)
+	art.cd(5, '', "\t<", "", False)
+	art.cd('light_cyan', '', "S", "", False)
+	art.cd(5, '', "> ", "", False)
+	art.cd(2, '', "Sector Editor", "", True)
 
-    # Row ??
-    print("")
-    art.cd(5, '', "\t<", "", False)
-    art.cd('226', '', "Q", "", False)
-    art.cd(5, '', "> ", "", False)
-    art.cd(2, '', "Quit Editor", "", True)
-    # Footer
-    art.cd("", "", "", "reset", True)
+	# Row ??
+	print("")
+	art.cd(5, '', "\t<", "", False)
+	art.cd('226', '', "Q", "", False)
+	art.cd(5, '', "> ", "", False)
+	art.cd(2, '', "Quit Editor", "", True)
+	# Footer
+	art.cd("", "", "", "reset", True)
 
 
 # The Main Menu
 adm_main_menu()
 command = ""
 while command != "q":
-    art.cd('light_cyan', '', "Enter your choice ", 'reset', False)
-    art.cd('226', '', "[?]", 'reset', False)
-    art.cd('light_cyan', '', ": ", 'reset', False)
-    command = input("")
-    match command:
-        case ('b' | 'B'):
-            # The Big Bang
-            bb.bb_main_menu()
-            # This will display the menu once you leave the Big Bang Editor.
-            adm_main_menu()
-        case ('s' | 'S'):
-            # Sector viewer.
-            pass
-        case ('?' | ''):
-            adm_main_menu()
-        case ('q' | 'Q'):
-            print("")
-            art.cd(0, 196, "  Live long and prosper  ", 'reset', True)
-            print("")
-            quit()
-        case _:
-            art.cd(1, '', 'Command not found. Please try again.', "reset", True)
-            print("")
+	art.cd('light_cyan', '', "Enter your choice ", 'reset', False)
+	art.cd('226', '', "[?]", 'reset', False)
+	art.cd('light_cyan', '', ": ", 'reset', False)
+	command = input("")
+	match command:
+		case ('b' | 'B'):
+			# The Big Bang
+			bb.bb_main_menu()
+			# This will display the menu once you leave the Big Bang Editor.
+			adm_main_menu()
+		case ('s' | 'S'):
+			# Sector viewer.
+			pass
+		case ('?' | ''):
+			adm_main_menu()
+		case ('q' | 'Q'):
+			print("")
+			art.cd(0, 196, "  Live long and prosper  ", 'reset', True)
+			print("")
+			quit()
+		case _:
+			art.cd(1, '', 'Command not found. Please try again.', "reset", True)
+			print("")
