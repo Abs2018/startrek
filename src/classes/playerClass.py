d3cfbea3e9e8a58a17709f8e0546c6e48de6587b
Alex Kurt <akurt@yellowcanary.ai>
2022-05-27 20:13:10 -0400

    Working on player creation

--- src/classes/playerClass.py
+++ src/classes/playerClass.py
@@ -1,20 +1,25 @@
 import mysql.connector
 from mysql.connector import Error
 from modules import db
+from modules import art
+
 
 class player():
-	def __init__(self):
-		pass
-	def playerCheck(self):
-		# Get the player name.
-		playername = input("What is your name? ")
-		# Check the DB for player.
-		connection = db.stdb()
-		query = "select * from `players` where `name` = '"+playername+"'"
-		results = db.rowcount(connection, query)
-		if results == 0:
-			# Register player
-			print("We need your name.")
-		else :
-			# Load player information
-			print("We found you.")
+    def __init__(self):
+        pass
+
+    def playerCheck(self):
+        # Get the player name.
+        art.cd(13, '', "", '', False)
+        playername = input("What is your name? ")
+        art.cd(4, '', "", 0, False)
+        # Check the DB for player.
+        connection = db.stdb()
+        query = "select * from `players` where `name` = '"+playername+"'"
+        results = db.rowcount(connection, query)
+        if results == 0:
+            # Register player
+            print("We need your name.")
+        else:
+            # Load player information
+            print("We found you.")
