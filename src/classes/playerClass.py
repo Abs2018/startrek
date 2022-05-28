78f7680575a17be259b83d3e85faa13132265d94
Alex Kurt <akurt@yellowcanary.ai>
2022-05-28 19:58:41 -0400

    Player is now saveable into the database.

--- src/classes/playerClass.py
+++ src/classes/playerClass.py
@@ -1,3 +1,4 @@
+from subprocess import call
 import mysql.connector
 from mysql.connector import Error
 from modules import db
@@ -11,44 +12,55 @@ class player():
     def check(self):
         # Get the player name.
         art.cd(5, '', "", '', False)
-        playername = input("What is your callsign? ")
+        callsign = ''
+        while True:
+            print("")
+            callsign = input("What is your callsign? ")
+            if callsign:
+                break
         art.cd(4, '', "", 0, False)
         # Check the DB for player.
         connection = db.stdb()
-        query = "select * from `players` where `callsign` = '"+playername+"'"
+        query = "select * from `players` where `callsign` = '"+callsign+"'"
         results = db.rowcount(connection, query)
         if results == 0:
             # Register player
             print("")
-            art.cd(2,'',"You were not found in the player database.",'',True)
-            art.cd(255,4,"Would you like to start a new character in this game? (Type ",'',False)
-            art.cd(3,'',"Y",'',False)
-            art.cd(255,4," or ",'',False)
-            art.cd(3,'',"N",'',False)
-            art.cd(255,4,")",0,False)
-            art.cd('cyan','','','',True)
+            art.cd(2, '', "You were not found in the player database.", '', True)
+            print("")
+            art.cd(
+                255, 4, "Would you like to start a new character in this game? (Type ", '', False)
+            art.cd(3, '', "Y", '', False)
+            art.cd(255, 4, " or ", '', False)
+            art.cd(3, '', "N", '', False)
+            art.cd(255, 4, ")", 0, False)
+            art.cd('cyan', '', '', '', True)
             command = input("")
             if command == "y" or command == "Y" or command == "":
                 # Create the player.
                 print("")
-                art.cd(3,'',"Great! ",'',False)
-                art.cd('cyan','',"Let's get some paperwork out of the way.",'',True)
+                art.cd(3, '', "Great! ", '', False)
+                art.cd(
+                    'cyan', '', "Let's get some paperwork out of the way.", '', True)
                 print("")
-                art.cd('cyan','',"What is your first name? ",0,True)
+                art.cd('cyan', '', "What is your first name? ", 0, True)
                 fname = input("")
-                art.cd('cyan','',"If you have a middle name, let me know. Press 'Enter' to skip. ",0,True)
+                art.cd(
+                    'cyan', '', "If you have a middle name, let me know. Press 'Enter' to skip. ", 0, True)
                 mname = input("")
-                art.cd('cyan','',"Last, but not least, what is your last name? ",0,True)
+                art.cd(
+                    'cyan', '', "Last, but not least, what is your last name? ", 0, True)
                 lname = input("")
                 branch = ""
                 while branch == "":
-                    art.cd('cyan','',"Which branch of service are you most interested in? (",'',False)
-                    art.cd(3,'',"Command",'',False)
-                    art.cd('cyan','',", ",'',False)
-                    art.cd(4,'',"Sciences",'',False)
-                    art.cd('cyan','',", or ",'',False)
-                    art.cd(1,'',"Operations",'',False)
-                    art.cd('cyan','',") ",0,True)
+                    art.cd(
+                        'cyan', '', "Which branch of service are you most interested in? (", '', False)
+                    art.cd(3, '', "Command", '', False)
+                    art.cd('cyan', '', ", ", '', False)
+                    art.cd(4, '', "Sciences", '', False)
+                    art.cd('cyan', '', ", or ", '', False)
+                    art.cd(1, '', "Operations", '', False)
+                    art.cd('cyan', '', ") ", 0, True)
                     branch = input("")
                     match branch:
                         case "Command":
@@ -64,11 +76,11 @@ class player():
                         case "operations":
                             branch = "Operations"
                         case _:
-                            branch=""
+                            branch = ""
                 # Save the player name.
                 print("")
-                art.cd('cyan','',"Give me a moment to sign you in...Okay, you're all set!",'',True)
-                userid="1"
+                art.cd(
+                    'cyan', '', "Give me a moment to sign you in...Okay, you're all set!", '', True)
                 alignment = "Federation"
                 rank = 1
                 xp = 0
@@ -83,12 +95,19 @@ class player():
                 birthday = "24/04"
                 homeplanet = "Earth"
                 languages = "English"
-                #p.playercreate(path,slash,userid,fname,mname,lname,alignment,rank,branch,xp,kills,deaths,locationx,locationy,whereami,health,species,age,birthday,homeplanet,languages)
+                player.create(callsign, fname, mname, lname, alignment, rank, branch, xp, kills, deaths,
+                              locationx, locationy, whereami, health, species, age, birthday, homeplanet, languages)
             elif command == "N" or command == "n":
                 print("")
-                art.cd(2,'',"Maybe next time...",0,True)
+                art.cd(2, '', "Maybe next time...", 0, True)
                 print("")
                 quit()
         else:
             # Load player information
             print("We found you.")
+
+    def create(callsign, fname, mname, lname, alignment, rank, branch, xp, kills, deaths, locationx, locationy, whereami, health, species, age, birthday, homeplanet, languages):
+        connection = db.stdb()
+        query = "INSERT INTO `players` (`callsign`, `fname`, `mname`, `lname`, `alignment`, `rank`, `branch`, `xp`, `kills`, `deaths`, `locationx`, `locationy`, `whereami`, `health`, `species`, `age`, `birthday`, `homeplanet`, `languages`) VALUES ('"+str(callsign)+"', '"+str(fname)+"', '"+str(mname)+"', '"+str(
+            lname)+"', '"+str(alignment)+"', '"+str(rank)+"', '"+str(branch)+"', '"+str(xp)+"', '"+str(kills)+"', '"+str(deaths)+"', '"+str(locationx)+"', '"+str(locationy)+"', '"+str(whereami)+"', '"+str(health)+"', '"+str(species)+"', '"+str(age)+"', '"+str(birthday)+"', '"+str(homeplanet)+"', '"+str(languages)+"')"
+        db.query(connection, query)
--- src/modules/setup.py
+++ src/modules/setup.py
@@ -27,7 +27,7 @@ def setup():
 
     # Create User Table
     connection = db.stdb()
-    query = "CREATE TABLE `players` (`pid` int(24) NOT NULL, `callsign` varchar(128) NOT NULL, `fname` varchar(128) NOT NULL, `mname` varchar(128) NOT NULL, `lname` varchar(128) NOT NULL, `alignement` varchar(128) NOT NULL,`rank` int(4) NOT NULL, `branch` int(3) NOT NULL, `xp` int(128) NOT NULL, `kills` int(128) NOT NULL, `deaths` int(128) NOT NULL,`locationx` int(24) NOT NULL,`locationy` int(24) NOT NULL, `whereami` int(24) NOT NULL, `health` int(24) NOT NULL, `species` int(24) NOT NULL, `age` int(24) NOT NULL, `birthday` int(24) NOT NULL, `homeplanet` int(24) NOT NULL, `languages` int(24) NOT NULL, `createdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, `lastlogin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP)"
+    query = "CREATE TABLE `players` (`pid` int(24) NOT NULL, `callsign` varchar(128) NOT NULL, `fname` varchar(128) NOT NULL, `mname` varchar(128) NOT NULL, `lname` varchar(128) NOT NULL, `alignment` varchar(128) NOT NULL,`rank` int(4) NOT NULL, `branch` varchar(16) NOT NULL, `xp` int(128) NOT NULL, `kills` int(128) NOT NULL, `deaths` int(128) NOT NULL,`locationx` int(24) NOT NULL,`locationy` int(24) NOT NULL, `whereami` varchar(24) NOT NULL, `health` int(24) NOT NULL, `species` varchar(24) NOT NULL, `age` int(24) NOT NULL, `birthday` varchar(24) NOT NULL, `homeplanet` varchar(24) NOT NULL, `languages` varchar(24) NOT NULL, `createdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, `lastlogin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP)"
     db.query(connection, query)
 
     connection = db.stdb()
@@ -143,7 +143,7 @@ def setup():
 
     art.cd(27, '', "Aligning planet orbits.", "reset", True)
 
-    # Create the Civilizations table table
+    # Create the Civilizations table
     connection = db.stdb()
     query = "CREATE TABLE `civilizations` (`cid` int(128) NOT NULL, `secid` int(128), `x` int(128) NOT NULL, `y` int(128) NOT NULL, `civname` varchar(128))"
     db.query(connection, query)
